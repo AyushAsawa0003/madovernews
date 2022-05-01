ef202114ba5b624e720c151467f784d512f24194
AyushAsawa0003 <77845546+AyushAsawa0003@users.noreply.github.com>
2022-05-01 14:40:25 +0530

    first commit

--- src/custom/DropDown/Dropdown.js
+++ src/custom/DropDown/Dropdown.js
@@ -3,21 +3,14 @@ import "./Dropdown.css";
 import { Link } from "react-router-dom";
 
 const items = [
-  "Trending",
-  "All News",
-  "Latest",
-  "India",
-  "World",
   "Business",
   "Sports",
   "Politics",
   "Technology",
-  "Startups",
   "Entertainment",
   "Science",
-  "Automobile",
-  "IPL",
-  "Soccer",
+  "General",
+  "Health",
 ];
 
 const Dropdown = ({ setSection, setShowCategory }) => {
